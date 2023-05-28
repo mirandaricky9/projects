7bc2fd72185915c7742cd28abfc2aa08118c9d2d
mirandaricky9 <mirari01@gettysburg.edu>
2023-05-28 16:29:45 -0700

    Adding another project to the repo

--- Project_2/main.py
+++ Project_2/main.py
@@ -1,1 +1,22 @@
 # Testing 
+import requests
+
+def run_query(query):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+
+
+new_query = """
+{
+    items(name: "m855a1") {
+        id
+    }
+}
+"""
+
+result = run_query(new_query)
+print(result)
